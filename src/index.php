6fbac8d6f766233c842b80e018f0004eb400ce51
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-03-15 10:48:21 +0530

    done

--- src/index.php
+++ src/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- src/view.php
+++ src/view.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_destroy();
 if(!isset($_SESSION['details']))
 {
     $_SESSION['details']=array();
@@ -21,27 +22,15 @@ if(isset($_SESSION['details']))
     {
 echo "Name is : $v[0]<br> Password is : $v[1]<br><br>";
     }
+    echo'<a href="index.php" name="LogOut">Click Here to logout</a>';
 }
 
-
-
 }
 
-
-
+if(isset($_POST['LogOut']))
+{
+    session_destroy();
+    echo 'destroy';
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <form action="index.php" method="POST">
-<input type="submit" name="submit" value="LogOut">
-</form> 
-</body>
-</html>
